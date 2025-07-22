f6c70221dd0217854907e925dac9b360a072401e
WR3NCH\emred <yunusemre.durak@icloud.com>
2025-07-22 14:51:57 +0300

    decision making practises finished

---
+++ 03_DecisionMaking/Program.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_DecisionMaking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region If Else Case
+            //string password;
+            //Console.WriteLine("***** If Case Example *****");
+            //Console.Write("Please Enter Your Password: ");
+            //password = Console.ReadLine();
+            //if (password == "test123")
+            //{
+            //    Console.WriteLine("Password is Correct!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Password is Incorrect!");
+            //}
+
+            #endregion
+
+            #region Grade Calculator Example
+
+
+            //Console.WriteLine("***** Grade Calculator *****");
+            //Console.WriteLine("Please Enter Your Grades Below: ");
+            //Console.WriteLine("Lab Grades effect on total grade is %30, Midterm Grades effect on total grades %30, Final Frades effect on total Grade is %40");
+
+            //int labGrade, finalGrade, midtermGrade, totalGrade;
+            //Console.Write("Please Enter Your Lab Grade: ");
+            //labGrade = int.Parse(Console.ReadLine());   
+            //Console.Write("Please Enter Your Midterm Grade: ");
+            //midtermGrade = int.Parse(Console.ReadLine());
+            //Console.Write("Please Enter Your Final Grade: ");
+            //finalGrade = int.Parse(Console.ReadLine()); 
+            //totalGrade = (labGrade * 30 / 100) + (midtermGrade * 30 / 100) + (finalGrade * 40 / 100);
+            //
+            //if (totalGrade < 0){
+            //    Console.WriteLine("Your Grade is Unnacceptable Please Check Again");
+            //      }
+            //if (totalGrade < 50){
+            //    Console.WriteLine("Your Total Grade is: " + totalGrade + " - You Failed the Course!");
+            //}
+            //else if (totalGrade >= 50 && totalGrade < 70)
+            //{
+            //    Console.WriteLine("Your Total Grade is: " + totalGrade + " - You Passed the Course with a C Grade!");
+            //}
+            //else if (totalGrade >= 70 && totalGrade < 85)
+            //{
+            //    Console.WriteLine("Your Total Grade is: " + totalGrade + " - You Passed the Course with a B Grade!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Your Total Grade is: " + totalGrade + " - You Passed the Course with an A Grade!");
+            //}
+            #endregion
+
+            #region Mod Calculations
+
+            //int number = 66;
+            //int result = number % 7;
+            //Console.WriteLine("The result of " + number + " % 7 is: " + result);
+
+            //Console.WriteLine("Please Enter Number 1: ");
+            //int number1 = int.Parse(Console.ReadLine());
+
+            //Console.Write("Please Enter Number 2: ");
+            //int number2 = int.Parse(Console.ReadLine());
+
+
+            //if (number2 == 0)
+            //{
+            //    Console.WriteLine("Division by zero is not allowed.");
+            //    return;
+            //}   
+            //int modResult = number1 % number2;
+            //Console.WriteLine("The result of " + number1 + " % " + number2 + " is: " + modResult);
+
+            #endregion
+
+            #region Mini Calculator With Switch Case
+            //int num1, num2, result;
+            //char symbol;
+
+            //Console.WriteLine("***** Mini Calculator *****");
+            //Console.WriteLine("Please Enter Number 1: ");   
+            //num1 = int.Parse(Console.ReadLine());
+            //Console.Write("Please Enter Number 2: ");
+            //num2 = int.Parse(Console.ReadLine());
+            //Console.Write("Please Enter a Symbol (+, -, *, /): ");
+            //symbol = char.Parse(Console.ReadLine());    
+            //switch (symbol)
+            //{
+            //    case '+':
+            //        result = num1 + num2;
+            //        Console.WriteLine("The result of " + num1 + " + " + num2 + " is: " + result);
+            //        break;
+            //    case '-':
+            //        result = num1 - num2;
+            //        Console.WriteLine("The result of " + num1 + " - " + num2 + " is: " + result);
+            //        break;  
+            //    case '*':
+            //        result = num1 * num2;
+            //        Console.WriteLine("The result of " + num1 + " * " + num2 + " is: " + result);
+            //        break;
+            //    case '/':
+            //        result = num1 / num2;   
+            //        Console.WriteLine("The result of " + num1 + " / " + num2 + " is: " + result);
+            //        break;  
+            //}
+
+
+
+            #endregion
+        }
+    }
+}
