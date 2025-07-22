277ac7f3bb1ea21d26b3d5fa4c181a6f87b46632
WR3NCH\emred <yunusemre.durak@icloud.com>
2025-07-22 13:44:12 +0300

    Varaibles file moved from Rider

--- 01-MainSubjects/Program.cs
+++ 01-MainSubjects/Program.cs
@@ -10,6 +10,143 @@ namespace _01_MainSubjects
     {
         static void Main(string[] args)
         {
+            #region Writing Methods
+
+            //Console.WriteLine("Merhaba Dünya");
+            //Console.Write("Selam");
+
+            //Console.WriteLine("***** Spotify Playlist *****");
+            //Console.WriteLine();
+            //Console.WriteLine("1-Daft Punk - Varidis Quo");
+            //Console.WriteLine("2-Kendrick Lamar - Count Me Out");
+            //Console.WriteLine("3-Drake feat. J. Cole - First Person Shooter");
+            //Console.WriteLine("4-Tame Impala - New Person Same Old Mistakes");
+            //Console.WriteLine("5-Şiwan Perwer - Xanıma Min");
+            //Console.WriteLine("6-Ciwan Haco - Kazacok");
+            //Console.WriteLine();
+            //Console.WriteLine("***************************");
+
+            #endregion
+
+            #region String Variables
+
+            //string
+            //Değişken_türü değişken_adı;
+
+            //string name;
+            //name = "Murat";
+            //Console.Write(name);
+
+            //string customerName;
+            //string customerSurname;
+            //string customerPhone;
+            //string customerEmail, district, city;
+
+            //customerName = "Hans";
+            //customerSurname = "Landa";
+            //customerPhone = "+90 500 400 30 20";
+            //customerEmail = "heildeutchland@gmail.com";
+            //district = "Germany";
+            //city = "Berlin";
+
+            //Console.WriteLine("**** Flight Attandance Card Generator ****");
+            //Console.WriteLine();
+            //Console.WriteLine("-------------------------------------------------");
+            //Console.WriteLine("Customer: " + customerName + " " + customerSurname);
+            //Console.WriteLine("İnformation: " + customerPhone);
+            //Console.WriteLine("Email Address: " + customerEmail);
+            //Console.WriteLine("Address: " + district + "/" + city);
+            //Console.WriteLine("-------------------------------------------------");
+
+            //Console.WriteLine();
+
+            //customerName = "Dominic";
+            //customerSurname = "Decoco";
+            //customerPhone = "+90 400 300 80 70";
+            //customerEmail = "forzaitalia@gmail.com";
+            //district = "İtalya";
+            //city = "Sicily";
+
+            //Console.WriteLine("-------------------------------------------------");
+            //Console.WriteLine("Customer: " + customerName + " " + customerSurname);
+            //Console.WriteLine("İnformation: " + customerPhone);
+            //Console.WriteLine("Email Address: " + customerEmail);
+            //Console.WriteLine("Address: " + district + "/" + city);
+            //Console.WriteLine("-------------------------------------------------");
+
+            #endregion
+
+            #region Int Variables
+
+            //int
+            //int number = 24;
+            //Console.WriteLine(number);
+
+            int hamburgerPrice = 300;
+            int cokePrice = 35;
+            int waterPrice = 10;
+            int friesPrice = 50;
+            int pizzaPrice = 250;
+            int lemonadePrice = 30;
+
+            Console.WriteLine("**** MENU  ****");
+            Console.WriteLine();
+            Console.WriteLine("-----Hamburger: " + hamburgerPrice + " TL");
+            Console.WriteLine("-----Pizza:     " + pizzaPrice + " TL");
+            Console.WriteLine("-----Coke:      " + cokePrice + " TL");
+            Console.WriteLine("-----Lemonade:  " + lemonadePrice + " TL");
+            Console.WriteLine("-----Fries:  " + friesPrice + " TL");
+            Console.WriteLine("-----Water: " + waterPrice + " TL");
+            Console.WriteLine();
+            Console.WriteLine("**** MENU ****");
+
+
+            Console.WriteLine();
+            int hamburgerCount;
+            int cokeCount;
+            int waterCount;
+            int friesCount;
+            int pizzaCount;
+            int lemonadeCount;
+
+            int totalHamgurgerPrice;
+            int totalCokePrice;
+            int totalWaterPrice;
+            int totalFriesPrice;
+            int totalPizzaPrice;
+            int totalLemonadePrice;
+
+            hamburgerCount = 3;
+            cokeCount = 3;
+            waterCount = 3;
+            friesCount = 1;
+            pizzaCount = 0;
+            lemonadeCount = 0;
+
+            totalHamgurgerPrice = hamburgerCount * hamburgerPrice;
+            totalCokePrice = cokeCount * cokePrice;
+            totalWaterPrice = waterCount * waterPrice;
+            totalLemonadePrice = lemonadeCount * lemonadePrice;
+            totalFriesPrice = friesCount * friesPrice;
+            totalPizzaPrice = pizzaCount * pizzaPrice;
+
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Hamburger Price: " + totalHamgurgerPrice + " TL");
+            Console.WriteLine("Pizza Price: " + totalPizzaPrice + " TL");
+            Console.WriteLine("Fries Price: " + totalFriesPrice + " TL");
+            Console.WriteLine("COoke Price: " + totalCokePrice + " TL");
+            Console.WriteLine("Lemonade Price: " + totalLemonadePrice + " TL");
+            Console.WriteLine("Water Price: " + totalWaterPrice + " TL");
+
+            Console.WriteLine();
+
+            int totalPrice = totalCokePrice + totalWaterPrice + totalLemonadePrice + totalHamgurgerPrice + totalPizzaPrice + totalFriesPrice;
+
+            Console.WriteLine("Total Price to Pay: " + totalPrice + " TL");
+
+            #endregion
+
+            Console.Read();
         }
     }
 }
---
+++ 02_Variables/Program.cs
@@ -0,0 +1,184 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Double Variables
+
+            //double number;
+
+            //number = 4.85;
+
+            //Console.WriteLine(number);
+
+            //Console.WriteLine("***** Fruit Price List *****");
+            //Console.WriteLine();
+
+            //double applePrice, orangePrice, strawberryPrice, potatoPrice, tomatoPrice;
+
+            //applePrice = 14.85;
+            //orangePrice = 20.95;
+            //strawberryPrice = 45;
+            //potatoPrice = 9.74;
+            //tomatoPrice = 6.88;
+
+            //Console.WriteLine("---- Single Apple Price:" + applePrice + " TL");
+            //Console.WriteLine("---- Single Orange Price:" + orangePrice + " TL");
+            //Console.WriteLine("---- Single Strawberry Price:" + strawberryPrice + " TL");
+            //Console.WriteLine("---- Single Potato Price :" + potatoPrice + " TL");
+            //Console.WriteLine("---- Single Tomato Price:" + tomatoPrice + " TL");
+
+            //Console.WriteLine();
+            //Console.WriteLine();
+
+            //double appleGram, orangeGram, strawberryGram, potatoGram, tomatoGram;
+
+            //appleGram = 1.245;
+            //orangeGram = 2.650;
+            //strawberryGram = 0.750;
+            //potatoGram = 4.859;
+            //tomatoGram = 3.745;
+
+            //double appleTotalPrice = appleGram * applePrice;
+            //double orangeTotalPrice = orangeGram * orangePrice;
+            //double strawberryTotalPrice = strawberryGram * strawberryPrice;
+            //double potatoTotalPrice = potatoGram * potatoPrice;
+            //double tomatoTotalPrice = tomatoGram * tomatoPrice;
+
+            //Console.WriteLine("Bought Fruit: Apple - " + "Single Price: " + applePrice + " - Gram: " + appleGram + " - Total Payment: " + appleTotalPrice);
+
+            //Console.WriteLine("Alınan Ürün: Portakal - " + "Single Price: " + orangePrice + " - Gram: " + orangeGram + " - Total Payment: " + orangeTotalPrice);
+
+            //Console.WriteLine("Alınan Ürün: Çilek - " + "Single Price: " + strawberryPrice + " - Gram: " + strawberryGram + " - Total Payment: " + strawberryTotalPrice);
+
+            //Console.WriteLine("Alınan Ürün: Patates - " + "Single Price: " + potatoPrice + " - Gram: " + potatoGram + " - Total Payment: " + potatoTotalPrice);
+
+            //Console.WriteLine("Alınan Ürün: Domates - " + "Single Price: " + tomatoPrice + " - Gram: " + tomatoGram + " - Total Payment: " + tomatoTotalPrice);
+
+            //double shoppingTotalPrice = appleTotalPrice + orangeTotalPrice + strawberryTotalPrice + tomatoTotalPrice + potatoTotalPrice;
+
+            //Console.WriteLine();
+            //Console.WriteLine();
+
+            //Console.WriteLine("All Total Payment:" + shoppingTotalPrice + " TL");
+
+            #endregion
+
+            #region Char Variables
+
+            //ABCDEFGH
+            //DEF...
+            //TOPLANTI SAAT 20:00'DE
+            //"  ' 
+
+            //char symbol;
+            //symbol = 'a';
+
+            //Console.WriteLine(symbol);
+
+            #endregion
+
+            #region Input String Data
+
+            //Console.WriteLine("**** Flight Attandance Generator ****");
+            //Console.WriteLine();
+
+            //string passengerName, passengerSurname, passengerDistrict, passengerCity, passengerAge, passengerIdentityNumber;
+
+            //Console.Write("Passanger Name: ");
+            //passengerName = Console.ReadLine();
+
+            //Console.Write("Passanger Surname: ");
+            //passengerSurname = Console.ReadLine();
+
+            //Console.Write("District Info: ");
+            //passengerDistrict = Console.ReadLine();
+
+            //Console.Write("City Info: ");
+            //passengerCity = Console.ReadLine();
+
+            //Console.Write("Passanger Age: ");
+            //passengerAge = Console.ReadLine();
+
+            //Console.Write("Passanger ID: ");
+            //passengerIdentityNumber = Console.ReadLine();
+
+            //Console.WriteLine();
+
+            //Console.WriteLine("-----------------------");
+            //Console.WriteLine("Passanger ID No: " + passengerIdentityNumber +  " - Passanger Name Surname: " + passengerName + " " + passengerSurname + " "  + passengerDistrict  + " / " + passengerCity + " " + passengerAge);
+
+            #endregion
+
+            #region Input Integer Data
+
+            //ABC12D
+
+            //int shoesPrice, computerPrice, chairPrice, tvPrice;
+            //shoesPrice = 1000;
+            //computerPrice = 20000;
+            //chairPrice = 5000;
+            //tvPrice = 12000;
+
+            //int shoesCount, computerCount, chairCount, tvCount;
+
+            //Console.Write("Please Verify how many shoes you bought: ");
+            //shoesCount = int.Parse(Console.ReadLine());
+
+            //Console.Write("Please Verify how many computers you bought: ");
+            //computerCount = int.Parse(Console.ReadLine());
+
+            //Console.Write("Please Verify how many chair you bought: ");
+            //chairCount = int.Parse(Console.ReadLine());
+
+            //Console.Write("Please Verify how many television you bought: ");
+            //tvCount = int.Parse(Console.ReadLine());
+
+            //int totalPrice = shoesCount * shoesPrice + computerCount * computerPrice + chairCount * chairPrice + tvCount * tvPrice;
+
+            //Console.WriteLine();
+            //Console.WriteLine("Total Bill to Pay " +  totalPrice);
+
+            #endregion
+
+            #region Input Double Data
+
+            //double exam1, exam2, exam3, result;
+
+            //Console.Write("Exam 1 Result: ");
+            //exam1 = double.Parse(Console.ReadLine());
+
+            //Console.Write("Exam 2 Result:  ");
+            //exam2 = double.Parse(Console.ReadLine());
+
+            //Console.Write("Exam 3 Result:  ");
+            //exam3 = double.Parse(Console.ReadLine());
+
+            //result = (exam1 + exam2 + exam3) / 3;
+
+            //Console.WriteLine();
+            //Console.WriteLine("Exam Average: " + result);
+
+            #endregion
+
+            #region Input Char Data
+            //char gender;
+            //Console.Write("Please choose a gender:  ");
+            //gender = char.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Gender Chosen: " + gender);
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
