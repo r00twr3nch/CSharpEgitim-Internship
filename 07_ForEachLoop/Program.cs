7f084f7fe2519a1cce9267e775425fcd4f0c2887
WR3NCH\emred <yunusemre.durak@icloud.com>
2025-07-23 13:31:38 +0300

    Foreach Loop with a Grading System Example

---
+++ 07_ForEachLoop/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ForEachLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region ForEach Loop Example
+
+            //string[] cities = {"Milano", "Rome", "Florence", "Pisa", "Sicily", "Napoli" };
+            //Console.WriteLine("Cities in Italy:");
+            //foreach (string city in cities)
+            //{
+            //    Console.WriteLine(city);
+            //}
+
+            //int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //Console.WriteLine("Even Numbers:");
+            //foreach(int num in numbers)
+            //{
+            //    if(num % 2 == 0)
+            //    {
+            //        Console.WriteLine(num);
+            //    }
+            //}
+
+            //int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //int total = 0;
+            //foreach (int num  in numbers)
+            //{
+            //    total += num;
+            //}
+            //Console.WriteLine("Total of numbers: " + total);
+
+            //List<string> fruits = new List<string> { "Apple", "Banana", "Cherry", "Date", "Elderberry" };
+            //Console.WriteLine("Fruits in the list:");
+            //foreach (string fruit in fruits)
+            //{
+            //    Console.WriteLine(fruit);
+            //}
+
+            //string word = "WubbaLubbaDubDub";
+
+            //foreach (char letter in word)
+            //{
+            //    Console.WriteLine(letter);
+            //}
+            #endregion
+
+            #region Example Grade Application
+
+            //Console.WriteLine("-------------------Grade List For Students-------------------");
+
+            //Console.WriteLine("Please Enter the Number of Students:");
+            //int numberOfStudents = int.Parse(Console.ReadLine());
+            //string[] students = new string[numberOfStudents];
+            //for (int i = 0; i < numberOfStudents; i++)
+            //{
+            //    Console.WriteLine($"Please Enter the Name of Student {i + 1}:");
+            //    students[i] = Console.ReadLine();
+            //}
+            //Console.WriteLine("-------------------------------------------------------------");
+            //int studentGrades = 3;
+            //for (int i = 0; i < numberOfStudents; i++)
+            //{
+            //    Console.WriteLine($"Please Enter the Grades for {students[i]}:");
+            //    int[] grades = new int[studentGrades];
+            //    for (int j = 0; j < studentGrades; j++)
+            //    {
+            //        Console.WriteLine($"Grade {j + 1}:");
+            //        grades[j] = int.Parse(Console.ReadLine());
+            //    }
+            //    // Calculate average
+            //    double average = grades.Average();
+
+            //    if (average > 60) // Assuming 60 is the passing grade
+            //    {
+            //        Console.WriteLine($"{students[i]} has an average grade of {average} and is above average. Passed the Class");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{students[i]} has an average grade of {average} and is below average. Failed the Class");
+            //    }
+            //}
+            //Console.WriteLine("-------------------------------------------------------------");
+            //Console.WriteLine("Thank you for using the Grade Application!"); 
+            #endregion
+
+        }
+    }
+}
---
+++ 07_ForEachLoop/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("07_ForEachLoop")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("07_ForEachLoop")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("f85d5eab-1c5d-44ed-8bae-6d2d437da0f5")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
