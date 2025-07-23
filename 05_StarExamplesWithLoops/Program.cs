3f562add8bfabbb226d0a74862a4011b31d567ad
WR3NCH\emred <yunusemre.durak@icloud.com>
2025-07-23 11:16:02 +0300

    Loop examples with Stars

---
+++ 05_StarExamplesWithLoops/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_StarExamplesWithLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region 10 Stars From Up to Down
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+            //Console.Read();
+
+            #endregion
+
+            #region 10 Stars in Same Line   
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.Write("*");
+            //}
+
+            #endregion
+
+            #region 10x10 Square with Stars 
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("**********");
+            //}
+            //
+            #endregion
+
+            #region Right Triangle with Stars
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    for (int j = 1; j <= i ; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Inverted Right Triangle with Stars  
+            //     for (int i = 10; i >= 1; i--)
+            //{
+            //        for (int j = 1; j <= i; j++)
+            //        {
+            //            Console.Write("*");
+            //    }
+            //        Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Baklava Slice With Stars
+            //int n = 5;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int j = n - 1; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= (2 * i) - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //for (int i = n-1; i >= 1; i--)
+            //{
+            //    for (int j = n-1; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= (2 * i) - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            #region Pyramid With Stars
+            //int n = 5;
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = n-i; j > 0 ; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= 2*i-1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            #region Reverse Pyramid With Stars
+            //int n = 5;
+            //for (int i = n; i >= 1; i--)
+            //{             
+            //    for (int j = n - i; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }              
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+            Console.Read();
+        }
+    }
+}
+
