25768cc5f2df10bcdeb53b8d4e249c697664ac9b
WR3NCH\emred <yunusemre.durak@icloud.com>
2025-07-24 11:56:10 +0300

    SQL CRUD examples

---
+++ 10_dbCRUD/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_dbCRUD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Add Category Example
+            //Console.WriteLine("Welcome to the Pizzeria of Luigi!");
+            //Console.WriteLine();
+
+            //Console.WriteLine("---------------------------------");
+            //Console.WriteLine("Please write the category you want to add:");    
+            //string categoryName = Console.ReadLine();
+
+            //SqlConnection connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Internshipdb;Integrated Security=True");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("INSERT INTO CategoryTable (CategoryName) VALUES (@CategoryName)", connection);
+            //command.Parameters.AddWithValue("@CategoryName", categoryName);
+            //command.ExecuteNonQuery();
+            //connection.Close();
+            //Console.WriteLine("Category added successfully!"); 
+            #endregion
+
+            #region Adding Product Example 
+            //string productName;
+            //decimal productPrice;
+            //bool productStatus;
+
+            //Console.WriteLine("Please Write the Product You Want to Add: ");
+            //productName = Console.ReadLine();
+            //Console.WriteLine("Please Enter the Price Of Product: ");
+            //productPrice = Convert.ToDecimal(Console.ReadLine());
+            //Console.WriteLine("Please Enter the Status of Product (True/False): ");
+            //productStatus = Convert.ToBoolean(Console.ReadLine());
+            //SqlConnection connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Internshipdb;Integrated Security=True");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("INSERT INTO ProductTable (ProductName, ProductPrice, ProductStatus) VALUES (@ProductName, @ProductPrice, @ProductStatus)", connection);
+            //command.Parameters.AddWithValue("@ProductName", productName);
+            //command.Parameters.AddWithValue("@ProductPrice", productPrice);
+            //command.Parameters.AddWithValue("@ProductStatus", productStatus);
+            //command.ExecuteNonQuery();
+            //connection.Close();
+            //Console.WriteLine("Product added successfully!");
+            //Console.Read();
+
+            #endregion
+
+            #region List İtems Example
+            //SqlConnection connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Internshipdb;Integrated Security=True");
+            //connection.Open();
+
+            //SqlCommand command = new SqlCommand("SELECT * FROM ProductTable", connection);
+            //SqlDataAdapter adapter = new SqlDataAdapter(command);
+            //DataTable dataTable = new DataTable();
+            //adapter.Fill(dataTable);
+
+            //foreach (DataRow row in dataTable.Rows)
+            //{
+            //      foreach (var item in row.ItemArray)
+            //      {
+            //          Console.Write(item.ToString() + " ");
+            //    }   
+            //      Console.WriteLine();
+            //}
+            //connection.Close();
+
+            //Console.Read();
+            #endregion
+
+            #region Delete Item Example
+
+            //Console.Write("Please Enter the Item You Want to Delete: ");
+            //int productId = int.Parse(Console.ReadLine());
+            //SqlConnection connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Internshipdb;Integrated Security=True");
+
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("Delete From ProductTable Where ProductId=@productId", connection);
+            //command.Parameters.AddWithValue("@productId", productId);
+            //command.ExecuteNonQuery();
+
+            //connection.Close();
+
+            //Console.WriteLine("The Item Deleted Successfully");
+
+            //Console.Read();
+
+
+            #endregion
+
+            #region Update Item Example
+            //Console.WriteLine("Id Of Item That Needs to Update: ");
+            //int productId = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Please Enter the New Name of Product: ");
+            //string  productName = Console.ReadLine();
+            //Console.WriteLine("Please Enter the New Price of Product: ");
+            //decimal productPrice = decimal.Parse(Console.ReadLine());
+            //Console.WriteLine("Please Enter the New Status of Product (True/False): ");
+            //bool productStatus = Convert.ToBoolean(Console.ReadLine());
+
+
+            //SqlConnection connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Internshipdb;Integrated Security=True");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("UPDATE ProductTable SET ProductName=@ProductName, ProductPrice=@ProductPrice, ProductStatus=@ProductStatus WHERE ProductId=@ProductID ", connection);
+            //command.Parameters.AddWithValue("@ProductID", productId);
+            //command.Parameters.AddWithValue("@ProductName", productName);
+            //command.Parameters.AddWithValue("@ProductPrice", productPrice);
+            //command.Parameters.AddWithValue("@ProductStatus", productStatus);
+            //command.ExecuteNonQuery();
+
+
+            //connection.Close();
+
+            //Console.WriteLine("The Item Updated Successfully");
+            //Console.Read();
+            #endregion
+        }
+    }
+}
---
+++ 10_dbCRUD/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("10_dbCRUD")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("10_dbCRUD")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("6f44f5e2-12c5-40a7-9b0d-778d68564505")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
